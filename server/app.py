f8bbea969799305043a15b6ea15f04ec9748bfca
Benjamin Zhang <bzhang7@outlook.com>
2016-04-29 10:14:16 -0400

    first successful migration

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
+import logging
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+logging.basicConfig(filename='app_log.txt', level=logging.DEBUG,
+                    format='%(asctime)s %(message)s')
+logger = logging.getLogger(__name__)
+
+
 @app.route('/')
 def index():
     return 'OMG, it worked!'
---
+++ server/config.py
@@ -0,0 +1,5 @@
+import local_config
+
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@localhost:3306/trip_fare' % (local_config.MYSQL_USERNAME, local_config.MYSQL_PASSWORD)
+

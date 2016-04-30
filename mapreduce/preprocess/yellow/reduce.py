12e0c41583885517d5eedf56db771351342d539a
Mengdi Wang <wangmengdi@gmail.com>
2016-04-30 19:50:00 -0400

    update reduce.py

--- mapreduce/preprocess/yellow/reduce.py
+++ mapreduce/preprocess/yellow/reduce.py
@@ -15,33 +15,34 @@ input:
 '''
 reduce:
 key
-0   vendorID 
-1   triptime
-2   passenger count
-3   trip distance
-4   Store_and_fwd_Y
-5   Store_and_fwd_N
-6   fare amount
-7   extra
-8   mta tax
-9   tip amount
-10  ehail fee
-11  improvement surcharge
-12  total amount
-13  payment 1
-14  payment 2
-15  payment 3
-16  payment 4
-17  payment 5
-18  payment 6
-19  trip type 0
-20  trip type 1
-21  trip type 2
-22  tollsAmount
-23  record count
+0   vendorIDis1
+1   vendorIDis2
+2   triptime
+3   passenger count
+4   trip distance
+5   Store_and_fwd_Y
+6   Store_and_fwd_N
+7   fare amount
+8   extra
+9   mta tax
+10  tip amount
+11  ehail fee
+12  improvement surcharge
+13  total amount
+14  payment 1
+15  payment 2
+16  payment 3
+17  payment 4
+18  payment 5
+19  payment 6
+20  trip type 0
+21  trip type 1
+22  trip type 2
+23  tollsAmount
+24  record count
 
 '''
-record_cnt = 24
+record_cnt = 25
 #input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
     try:
@@ -50,24 +51,24 @@ for line in sys.stdin:
         (ratetype, value) = value_items2.split(',', 1)
         if rowtype == '1': #green
             if key in green[ratetype]:
-                green[ratetype][key][record_cnt-1] += 1
-                green[ratetype][key][0]  += float(value[0]) #triptime
-                green[ratetype][key][1]  += float(value[7]) #passenger count
-                green[ratetype][key][2]  += float(value[8]) #trip distance
-                if value[] == 'Y':
-                    green[ratetype][key][3]  += 1 #Store_and_fwd_Y
+                green[ratetype][key][int(value[0])-1]  += 1
+                green[ratetype][key][2]  += float(value[1]) #triptime
+                green[ratetype][key][3]  += float(value[7]) #passenger count
+                green[ratetype][key][4]  += float(value[8]) #trip distance
+                if value[2] == 'Y':
+                    green[ratetype][key][5]  += 1 #Store_and_fwd_Y
                 else:
-                    green[ratetype][key][4]  += 1 #Store_and_fwd_N
-                green[ratetype][key][5]  += float(value[9]) #fare amount
-                green[ratetype][key][6]  += float(value[10]) #extra
-                green[ratetype][key][7]  += float(value[11]) #mta tax
-                green[ratetype][key][8]  += float(value[12]) #tip amount
-                green[ratetype][key][9]  += float(value[13]) #ehail fee
-                green[ratetype][key][10] += float(value[14]) #improvement surcharge
-                green[ratetype][key][11] += float(value[15]) #total amount
-                payoffset = int(value[15])
-                tripoffset = int(value[16])
-                green[ratetype][key][11+payoffset] += 1
+                    green[ratetype][key][6]  += 1 #Store_and_fwd_N
+                green[ratetype][key][7]  += float(value[9]) #fare amount
+                green[ratetype][key][8]  += float(value[10]) #extra
+                green[ratetype][key][9]  += float(value[11]) #mta tax
+                green[ratetype][key][10] += float(value[12]) #tip amount
+                green[ratetype][key][11] += float(value[13]) #ehail fee
+                green[ratetype][key][12] += float(value[14]) #improvement surcharge
+                green[ratetype][key][13] += float(value[15]) #total amount
+                payoffset = int(value[16])
+                tripoffset = int(value[17])
+                green[ratetype][key][13+payoffset] += 1
                 '''
                 green[ratetype][key][12] += float() #payment 1
                 green[ratetype][key][13] += float() #payment 2
@@ -76,39 +77,54 @@ for line in sys.stdin:
                 green[ratetype][key][16] += float() #payment 5
                 green[ratetype][key][17] += float() #payment 6
                 '''
-                green[ratetype][key][18+tripoffset] += 1
+                green[ratetype][key][20+tripoffset] += 1
                 '''
                 green[ratetype][key][18] += float() #trip type 0
                 green[ratetype][key][19] += float() #trip type 1
                 green[ratetype][key][20] += float() #trip type 2
                 '''
-                green[ratetype][key][21] += float(0) #tollsAmount 
+                green[ratetype][key][23] += float(0) #tollsAmount 
+                green[ratetype][key][record_cnt-1] += 1
                                
             else:
                 row = [] * record_cnt
-                row[0]  = float() #vendorID
-                row[1]  = float() #triptime
-                row[2]  = float() #passenger count
-                row[3]  = float() #trip distance
-                row[4]  = float() #Store_and_fwd_Y
-                row[5]  = float() #Store_and_fwd_N
-                row[6]  = float() #fare amount
-                row[7]  = float() #extra
-                row[8]  = float() #mta tax
-                row[9]  = float() #tip amount
-                row[10] = float() #ehail fee
-                row[11] = float() #improvement surcharge
-                row[12] = float() #total amount
-                row[13] = float() #payment 1
-                row[14] = float() #payment 2
-                row[15] = float() #payment 3
-                row[16] = float() #payment 4
-                row[17] = float() #payment 5
-                row[18] = float() #payment 6
-                row[19] = float() #trip type 0
-                row[20] = float() #trip type 1
-                row[21] = float() #trip type 2
-                row[22] = float() #tollsAmount  
+                row[int(value[0]) - 1]  = 1
+                row[2]  = float(value[1]) #triptime
+                row[3]  = float(value[7]) #passenger count
+                row[4]  = float(value[8]) #trip distance
+                if value[2] == 'Y':
+                    row[5] = 1
+                else:
+                    row[6] = 1
+                '''
+                row[5]  = float() #Store_and_fwd_Y
+                row[6]  = float() #Store_and_fwd_N
+                '''
+                row[7]  = float(value[9]) #fare amount
+                row[8]  = float(value[10]) #extra
+                row[9]  = float(value[11]) #mta tax
+                row[10] = float(value[12]) #tip amount
+                row[11] = float(value[13]) #ehail fee
+                row[12] = float(value[14]) #improvement surcharge
+                row[13] = float(value[15]) #total amount
+                payoffset = int(value[16])
+                tripoffset = int(value[17])
+                row[13+payoffset] += 1
+                '''
+                row[14] = float() #payment 1
+                row[15] = float() #payment 2
+                row[16] = float() #payment 3
+                row[17] = float() #payment 4
+                row[18] = float() #payment 5
+                row[19] = float() #payment 6
+                '''
+                row[20+tripoffset] += 1
+                '''
+                row[20] = float() #trip type 0
+                row[21] = float() #trip type 1
+                row[22] = float() #trip type 2
+                '''
+                row[23] = 0.0 #tollsAmount  
                 row[record_cnt-1] = 1 #cnt  
                 green[ratetype][key] = row
                 

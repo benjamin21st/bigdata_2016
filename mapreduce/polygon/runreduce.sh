1c1db38d18511ef1f2b1c5e9d9411c878ff8fd30
Mengdi Wang <mw3265@login-2-1.local>
2016-05-10 15:12:50 -0400

    new file:   ../mapreduce/polygon/runreduce.sh

---
+++ mapreduce/polygon/runreduce.sh
@@ -0,0 +1,13 @@
+python2 reduce.py < ../../data/yellow_2015_01_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_01.txt
+python2 reduce.py < ../../data/yellow_2015_02_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_02.txt
+python2 reduce.py < ../../data/yellow_2015_03_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_03.txt
+python2 reduce.py < ../../data/yellow_2015_04_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_04.txt
+python2 reduce.py < ../../data/yellow_2015_05_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_05.txt
+python2 reduce.py < ../../data/yellow_2015_06_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_06.txt
+python2 reduce.py < ../../data/yellow_2015_07_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_07.txt
+python2 reduce.py < ../../data/yellow_2015_08_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_08.txt
+python2 reduce.py < ../../data/yellow_2015_09_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_09.txt
+python2 reduce.py < ../../data/yellow_2015_10_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_10.txt
+python2 reduce.py < ../../data/yellow_2015_11_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_11.txt
+python2 reduce.py < ../../data/yellow_2015_12_poly.txt > ../../mapreduceresult/YellowPoly/yellow_2015_12.txt
+
